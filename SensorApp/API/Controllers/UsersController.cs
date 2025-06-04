ef06cfec7c280e2a1d2ce608208387e30aa97a3a
qubit07 <qubit07@gmail.com>
2025-06-04 20:19:41 +0200

    add users controller

---
+++ SensorApp/API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(ILogger<UsersController> logger, DataContext dataContext) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await dataContext.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await dataContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
